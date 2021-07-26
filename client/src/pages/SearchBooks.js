b47f80d7cd528d2ca4c7fec353a87ce2bb831c60
mythosmystery <axiysfire@gmail.com>
2021-07-26 04:06:25 -0500

    bug fixes

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -58,20 +58,10 @@ const SearchBooks = () => {
       // find the book in `searchedBooks` state by the matching id
       const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-      // get token
-      const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-      if (!token) {
-         return false;
-      }
-
       try {
-         //  console.log({ ...bookToSave });
          const { data } = await saveBook({
             variables: { book: { ...bookToSave } },
          });
-         console.log(data);
-         // if book successfully saves to user's account, save book id to state
          data ? setSavedBookIds([...savedBookIds, bookToSave.bookId]) : console.err(error);
       } catch (err) {
          console.error(err);
